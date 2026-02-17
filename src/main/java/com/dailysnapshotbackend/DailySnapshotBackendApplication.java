fb1796ce6e13fcec25ad6533e443ba861283606b
AtanasRaev <atanaspraev@gmail.com>
2026-02-17 20:21:09 +0200

    First commit

---
+++ src/main/java/com/dailysnapshotbackend/DailySnapshotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dailysnapshotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySnapshotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailySnapshotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailysnapshotbackend/DailySnapshotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailysnapshotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySnapshotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
