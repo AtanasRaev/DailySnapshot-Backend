311cb1dc517d040ac78e92b4f93dac658fe9aeb4
AtanasRaev <atanaspraev@gmail.com>
2026-02-17 22:37:45 +0200

    Add AppConfig with ModelMapper and RestClient beans

---
+++ src/main/java/com/dailysnapshotbackend/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.dailysnapshotbackend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestClient genericRestClient() {
+        return RestClient.create();
+    }
+}
