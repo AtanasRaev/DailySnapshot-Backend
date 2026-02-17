9f272f1d110af2320589ae6ca4589f747717a32a
AtanasRaev <atanaspraev@gmail.com>
2026-02-17 22:38:29 +0200

    Remove unnecessary blank line in main application class

--- src/main/java/com/dailysnapshotbackend/DailySnapshotBackendApplication.java
+++ src/main/java/com/dailysnapshotbackend/DailySnapshotBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DailySnapshotBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DailySnapshotBackendApplication.class, args);
     }
