c65bc38ac3e5d796b2e03f1d440a6d116a9ab93d
AtanasRaev <atanaspraev@gmail.com>
2026-02-18 16:59:37 +0200

    Update WebController to add city search endpoint and enhance weather endpoint parameters

--- src/main/java/com/dailysnapshotbackend/web/WebController.java
+++ src/main/java/com/dailysnapshotbackend/web/WebController.java
@@ -1,12 +1,16 @@
 package com.dailysnapshotbackend.web;
 
+import com.dailysnapshotbackend.dto.CityDataDTO;
 import com.dailysnapshotbackend.dto.WeatherDataDTO;
 import com.dailysnapshotbackend.service.WeatherService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -14,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebController {
     private final WeatherService weatherService;
 
+    @GetMapping("/search")
+    public ResponseEntity<List<CityDataDTO>> search(@RequestParam("name") String input) {
+        return ResponseEntity.ok(this.weatherService.getCity(input));
+    }
+
     @GetMapping("/weather")
-    public ResponseEntity<WeatherDataDTO> getWeather() {
-        return ResponseEntity.ok(this.weatherService.getWeatherData(42.1354, 24.7453, "Plovdiv"));
+    public ResponseEntity<WeatherDataDTO> getWeather(@RequestParam("latitude") double latitude,
+                                                     @RequestParam("longitude") double longitude,
+                                                     @RequestParam("city") String city) {
+        return ResponseEntity.ok(this.weatherService.getWeatherData(latitude, longitude, city));
     }
 }
